b8e346fe112aa7104c0449e1828cffb3745c865e
beetix <b.t.freeman16@gmail.com>
2016-04-12 09:20:48 +0200

    Ajout constructeurs, getters, setters et toString pour HommePolitique

--- src/fichiersPourTp/tp1/HommePolitique.java
+++ src/fichiersPourTp/tp1/HommePolitique.java
@@ -16,5 +16,54 @@ public class HommePolitique {
     
     private String nom;
     private String nomParti;
+
+    public HommePolitique(Civilite civilite, String nom, String nomParti) {
+        this.civilite = civilite;
+        this.nom = nom;
+        this.nomParti = nomParti;
+    }
+
+    public Civilite getCivilite() {
+        return civilite;
+    }
+
+    public void setCivilite(Civilite civilite) {
+        this.civilite = civilite;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNomParti() {
+        return nomParti;
+    }
+
+    public void setNomParti(String nomParti) {
+        this.nomParti = nomParti;
+    }
+
+    @Override
+    public String toString() {
+        return "HommePolitique{" + "civilite=" + civilite + ", nom=" + nom + ", nomParti=" + nomParti + '}';
+    }
+    
+    
+    public static void main(String args[])
+    {
+        HommePolitique h1, h2, h3;
+        h1 = new HommePolitique(Civilite.FEMME,"nom1","parti1");
+        h3 =new HommePolitique(Civilite.HOMME,"nom3","parti3");
+        System.out.println("h1: " + h1);
+        
+        System.out.println("Est ce que civilite est correcte ? " + h1.getCivilite());
+        h1.setCivilite(Civilite.HOMME);
+        System.out.println("Est ce que civilite a changé ? " + h1.getCivilite());
+    }
+    
     
 }
