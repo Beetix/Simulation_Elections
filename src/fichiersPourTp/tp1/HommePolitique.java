e633ff7d42304660ef595c805ad6ed21d0f5fa9a
beetix <b.t.freeman16@gmail.com>
2016-04-12 09:04:50 +0200

    Déplacement des sources dans un package TP1

---
+++ src/fichiersPourTp/tp1/HommePolitique.java
@@ -0,0 +1,20 @@
+package fichiersPourTp.tp1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author beetix
+ */
+public class HommePolitique {
+    
+    private Civilite civilite;
+    
+    private String nom;
+    private String nomParti;
+    
+}
