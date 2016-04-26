57736561e12c6e9c962491fd355dc8c9d00fc1ec
loupiotte93 <lm.desuremain@gmail.com>
2016-04-26 17:40:00 +0200

    TP3 //

--- src/travail/tp2/Election.java
+++ src/travail/tp2/Election.java
@@ -14,7 +14,12 @@ import java.util.Random;
 
 import correction.tp1.Civilite;
 import correction.tp1.HommePolitique;
+import java.util.Collections;
 import java.util.ListIterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 
 
@@ -76,9 +81,134 @@ public class Election {
 		System.out.println(scrutin);
                 
                 List<Candidat> lc1 = scrutin.getResultatScrutin();
-                lc1.sort(new ComparateurCandidat());
+                System.out.println("Sans Tri");
                 afficheListe(lc1);
+                Collections.sort(lc1);
+                System.out.println("Tri alphabétique (ordre naturel)");
+                afficheListe(lc1);
+                System.out.println("Tri par pourcentage décroissant");
+                //lc1.sort(new ComparateurCandidat());
+                Collections.sort(lc1,new ComparateurCandidat());
+                afficheListe(lc1);
+                System.out.println("Tri par pourcentage croissant méthode 1 ");
+                Collections.sort(lc1,Collections.reverseOrder(new ComparateurCandidat()));
+                afficheListe(lc1);
+                System.out.println("Tri par pourcentage croissant méthode 2 ");
+                Collections.sort(lc1,new ComparateurCandidat());
+                Collections.reverse(lc1);
+                afficheListe(lc1);
+                // Récupération du meilleur candidat :
+                Collections.reverse(lc1);
+                System.out.println("Meileur candidat way 1 : " + lc1.get(0));
+                System.out.println("Meileur candidat way 2 : " + Collections.min(lc1, new ComparateurCandidat()) );
+                
+                System.out.println("Deuxième candidat way 1 : " + lc1.get(1));
+                System.out.println("Index du meilleur candidat via List : " +lc1.indexOf(Collections.max(lc1, new ComparateurCandidat())));
+                //List lc2=(ArrayList)((ArrayList)lc1).clone();
+                List<Candidat> lc2=new ArrayList<>(lc1);
+                System.out.println("Copie  ");
+                afficheListe(lc2);
+                
+                for( Candidat candidat : lc1 )
+                {
+                    if(candidat.getPourCentVoix()<20)
+                    { 
+                        lc2.remove(candidat);
+                    }
+                }
+                System.out.println("Après remove boucle for  ");
+                afficheListe(lc2);
+                
+                List<Candidat> lc3=new ArrayList<>(lc1);
+                for (ListIterator<Candidat> ite = lc1.listIterator(); ite.hasNext(); )
+                {
+                    Candidat c = ite.next();
+                    if(c.getPourCentVoix() <20)
+                    {
+                        lc3.remove(c);
+                    }
+                }
+                
+                System.out.println("Après remove iterator  ");
+                afficheListe(lc3);
+                
+                // 2,5
+                System.out.println("lc1  ");
+                afficheListe(lc1);
+                System.out.println("lc1 sans lc3  ");
+                lc1.retainAll(lc3);
+                afficheListe(lc1);
+                
+                
+                // 2,6
+                System.out.println("listes identiques equals: " + lc1.equals(lc3));
+                System.out.println("listes identiques contains : " + (lc1.containsAll(lc3) && lc3.containsAll(lc1)  ) );
                
+                System.out.println("Clear & Affichage de lc3 : ");
+                lc3.clear();
+                afficheListe(lc3);
+                System.out.println("lc3 is empty ? : " + lc3.isEmpty());
+                System.out.println("lc3 size ? : " + lc3.size());
+
+                
+                
+                //Exercie 3
+                List<Candidat> lc4 = scrutin.getResultatScrutin();
+                Map<Civilite,List<String>> map1 = new TreeMap<>();
+                
+                List<String> lh = new ArrayList<String>();
+                List<String> lf = new ArrayList<String>();
+                
+                map1.put(Civilite.HOMME, lh);
+                map1.put(Civilite.FEMME, lf);
+                
+                int nbhomme=0;
+                for (ListIterator<Candidat> ite = lc4.listIterator(); ite.hasNext(); )
+                {
+                    Candidat c = ite.next();
+                    if(c.getCivilite() == Civilite.HOMME )
+                    {
+                        lh.add(c.getNom());
+                        nbhomme++;
+                    }
+                    else
+                    {
+                        lf.add(c.getNom());
+                    }
+                }
+                
+                System.out.println(map1);
+                
+                Set< Map.Entry<Civilite, List<String> > > set1 = map1.entrySet();
+                
+                for (Map.Entry<Civilite, List<String> > e : set1)
+                {
+                    System.out.println("Liste des "+e.getKey() + " \n " );
+                    for (String s : e.getValue())
+                    {
+                        System.out.println(s+"\n");
+                    }
+                }
+                
+                System.out.println(set1);
+                
+                
+                System.out.println("Nombre d'hommes :" + nbhomme );
+                
+                Set<Civilite> set2 = map1.keySet();
+                System.out.println(set2);
+                
+                map1.remove(Civilite.FEMME);
+                System.out.println(map1);
+                System.out.println(set1); // Set aussi modifié car c'est une "vue" de la map.
+                System.out.println(set2);
+                
+                
+                Set<Civilite> monSet = new TreeSet(set2);
+                //monSet.add(Civilite.INCONNU);
+                monSet.add(Civilite.HOMME);
+                
+                
 	}
 
         private static void afficheListe(List<Candidat> liste)
