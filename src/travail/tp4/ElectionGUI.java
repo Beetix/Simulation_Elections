bec11baef593b6b6a9efcbc4c4da4adc70cf6cc1
loupiotte93 <lm.desuremain@gmail.com>
2016-05-19 11:32:34 +0200

    All charts with data :-)

--- src/travail/tp4/ElectionGUI.java
+++ src/travail/tp4/ElectionGUI.java
@@ -26,8 +26,34 @@ import static javax.swing.SwingConstants.WEST;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 import correction.tp1.Candidat;
+import correction.tp1.Civilite;
+import java.awt.Font;
+import java.util.Iterator;
 import java.util.Map;
 import javax.swing.Box;
+
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+
+import org.jfree.chart.renderer.category.BarRenderer;
+import java.awt.GradientPaint;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+
 
 /**
  *
@@ -119,10 +145,192 @@ public class ElectionGUI extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
     }
-    
-    public static void main(String[] args) {
-        ElectionGUI testElectionGUI = new ElectionGUI();
+
+    private JPanel remplirPourcentagePannel(List<Candidat> listcandidats , int dateScrutin) {
+        
+        String libelle = "Répartition des candidats du scrutin du "+dateScrutin;
+        
+        JPanel chart = new ChartPanel( createPieChart(createDataset(listcandidats),libelle));
+        
+        
+        return chart;
     }
+    
+    private static JFreeChart createPieChart(PieDataset dataset, String libelle) {
+        
+        JFreeChart chart = ChartFactory.createPieChart(
+            libelle,  // chart title
+            dataset,             // data
+            false,               // exclude legend
+            true,
+            false
+        );
+
+        PiePlot plot = (PiePlot) chart.getPlot();
+        plot.setLabelFont(new Font("SansSerif", Font.PLAIN, 12));
+        plot.setNoDataMessage("No data available");
+        plot.setCircular(false);
+        plot.setLabelGap(0.02);
+        return chart;
+        
+    }
+    
+        /**
+     * Creates a sample chart.
+     * 
+     * @param dataset  the dataset.
+     * 
+     * @return The chart.
+     */
+    private JFreeChart createBarChart(final CategoryDataset dataset, String libelle) {
+        
+        final JFreeChart chart = ChartFactory.createBarChart(
+            libelle,         // chart title
+            "",               // domain axis label
+            "Pourcentage",                  // range axis label
+            dataset,                  // data
+            PlotOrientation.VERTICAL, // orientation
+            true,                     // include legend
+            true,                     // tooltips?
+            false                     // URLs?
+        );
+
+        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+
+        // set the background color for the chart...
+        chart.setBackgroundPaint(Color.white);
+
+        // get a reference to the plot for further customisation...
+        final CategoryPlot plot = chart.getCategoryPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+
+        // set the range axis to display integers only...
+        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+
+        // disable bar outlines...
+        final BarRenderer renderer = (BarRenderer) plot.getRenderer();
+        renderer.setDrawBarOutline(false);
+        
+        // set up gradient paints for series...
+        final GradientPaint gp0 = new GradientPaint(
+            0.0f, 0.0f, Color.blue, 
+            0.0f, 0.0f, Color.lightGray
+        );
+        final GradientPaint gp1 = new GradientPaint(
+            0.0f, 0.0f, Color.green, 
+            0.0f, 0.0f, Color.lightGray
+        );
+        final GradientPaint gp2 = new GradientPaint(
+            0.0f, 0.0f, Color.red, 
+            0.0f, 0.0f, Color.lightGray
+        );
+        renderer.setSeriesPaint(0, gp0);
+        renderer.setSeriesPaint(1, gp1);
+        renderer.setSeriesPaint(2, gp2);
+
+        final CategoryAxis domainAxis = plot.getDomainAxis();
+        domainAxis.setCategoryLabelPositions(
+            CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 6.0)
+        );
+        // OPTIONAL CUSTOMISATION COMPLETED.
+        
+        return chart;
+        
+    }
+    
+    
+    private static PieDataset createDataset(List<Candidat> listecandidats) {
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        for(Candidat candidat : listecandidats)
+        {
+            dataset.setValue(String.format("%s ( %.2f %%)",candidat.getNom(),candidat.getPourCentVoix()),candidat.getPourCentVoix());
+        }
+        return dataset;        
+    }
+    
+    private static PieDataset createDataset(Map<String, List<Candidat>> mapParti) {
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        Iterator<Map.Entry<String, List<Candidat>>> iterator = mapParti.entrySet().iterator();
+        while ( iterator.hasNext()) {
+            
+            Map.Entry<String, List<Candidat>> entree = iterator.next();
+            List<Candidat> listeCandidats = entree.getValue();
+            
+            double pourcentageParti = 0;
+            
+            for (Candidat candidat : listeCandidats)
+            {
+                pourcentageParti += candidat.getPourCentVoix();
+            }
+            
+            dataset.setValue(String.format("%s ( %.2f %%)", entree.getKey() , pourcentageParti ), pourcentageParti);
+        }
+        
+        return dataset;        
+    }
+    
+    private CategoryDataset createSexeCandidatDataset(Map<Civilite, List<Candidat>> mapCiviliteCandidats) {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        Iterator<Map.Entry<Civilite, List<Candidat>>> iterator = mapCiviliteCandidats.entrySet().iterator();
+        while ( iterator.hasNext()) {
+            Map.Entry<Civilite, List<Candidat>> entree = iterator.next();
+            List<Candidat> listeCandidats = entree.getValue();
+            for (Candidat candidat : listeCandidats)
+            {
+                dataset.setValue(candidat.getPourCentVoix(),candidat.getCivilite() ,candidat.getNom());
+            }
+         }
+        return dataset;       
+    }
+    
+        private CategoryDataset createCandidatSexeDataset(Map<Civilite, List<Candidat>> mapCiviliteCandidats) {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        Iterator<Map.Entry<Civilite, List<Candidat>>> iterator = mapCiviliteCandidats.entrySet().iterator();
+        while ( iterator.hasNext()) {
+            Map.Entry<Civilite, List<Candidat>> entree = iterator.next();
+            List<Candidat> listeCandidats = entree.getValue();
+            for (Candidat candidat : listeCandidats)
+            {
+                dataset.setValue(candidat.getPourCentVoix(),candidat.getNom(),candidat.getCivilite());
+            }
+         }
+        return dataset;       
+    }
+
+
+    private JPanel remplirPartiPannel(List<Candidat> listcandidats, Map<String, List<Candidat>> newMapPartiCandidats, int dateScrutin) {
+        String libelle = "Répartition des candidats du scrutin du "+dateScrutin+" par parti.";
+        
+        JPanel chart = new ChartPanel( createPieChart(createDataset(newMapPartiCandidats),libelle));
+        
+        
+        return chart;   
+    }
+
+
+    private JPanel remplirSexeCandidatPannel(List<Candidat> listcandidats, Map<Civilite, List<Candidat>> newMapCiviliteCandidats, int dateScrutin) {
+         String libelle = "Répartition des candidats du scrutin du "+dateScrutin+" par Sexe/Candidat.";
+        
+        JPanel chart = new ChartPanel( createBarChart(createSexeCandidatDataset(newMapCiviliteCandidats),libelle));
+        
+        
+        return chart; 
+    }
+
+    private JPanel remplirCandidatSexePannel(List<Candidat> listcandidats, Map<Civilite, List<Candidat>> newMapCiviliteCandidats, int dateScrutin) {
+        String libelle = "Répartition des candidats du scrutin du "+dateScrutin+" par Candidat/Sexe.";
+        
+        JPanel chart = new ChartPanel( createBarChart(createCandidatSexeDataset(newMapCiviliteCandidats),libelle));
+        
+        
+        return chart; 
+    }
+
+
+    
 
     class DisplayAlphaOrderListener implements ActionListener {
         @Override
@@ -155,7 +363,7 @@ public class ElectionGUI extends JFrame {
     {
         // Panneau Ouest
         JPanel panneauOuest = new JPanel(new BorderLayout());
-        panneauOuest.setBackground(Color.yellow);
+        panneauOuest.setBackground(Color.white);
         panneauOuest.setBorder(new LineBorder(Color.cyan));
         
         // Label panneau Ouest
@@ -171,7 +379,7 @@ public class ElectionGUI extends JFrame {
             JLabel civ = new JLabel(candidat.getCivilite() + "");
             JLabel nom = new JLabel(candidat.getNom());
             JLabel parti = new JLabel(candidat.getParti());
-            JLabel voix = new JLabel(candidat.getPourCentVoix()+ " % des voix");
+            JLabel voix = new JLabel(String.format("%.2f %% des voix",candidat.getPourCentVoix()));
             infos.add(civ);
             infos.add(nom);
             infos.add(parti);
@@ -200,16 +408,28 @@ public class ElectionGUI extends JFrame {
         panneauCentral.setBackground(Color.red);
         panneauCentral.setBorder(new LineBorder(Color.white));
         
+        // Répartition des candidats du scrutin
+        JPanel pieparpourcentage =remplirPourcentagePannel(listcandidats, election.getDateSrutin());
+                
+        // Répartition des candidats du scrutin par parti 
+        JPanel pieparparti =remplirPartiPannel(listcandidats,election.newMapPartiCandidats(), election.getDateSrutin());
+        
+        // Répartition des candidats du scrutin par parti 
+        JPanel barreparcandidat =remplirSexeCandidatPannel(listcandidats,election.newMapCiviliteCandidats(), election.getDateSrutin());
+        
+        // Répartition des candidats du scrutin par parti 
+        JPanel barreparcivilite =remplirCandidatSexePannel(listcandidats,election.newMapCiviliteCandidats(), election.getDateSrutin());
+                
         // Labels panneau Central
         JLabel labelCentral1 = new JLabel("Centre 1");
         JLabel labelCentral2 = new JLabel("Centre 2");
         JLabel labelCentral3 = new JLabel("Centre 3");
         JLabel labelCentral4 = new JLabel("Centre 4");
         
-        panneauCentral.add(labelCentral1);
-        panneauCentral.add(labelCentral2);     
-        panneauCentral.add(labelCentral3);
-        panneauCentral.add(labelCentral4);
+        panneauCentral.add(pieparpourcentage);
+        panneauCentral.add(pieparparti);     
+        panneauCentral.add(barreparcandidat);
+        panneauCentral.add(barreparcivilite);
         
         // Ajout dans le panneau principal
         panneauPrincipal.setLayout(new BorderLayout());
